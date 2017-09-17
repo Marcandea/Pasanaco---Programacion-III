3c6fa7f559b3e9ea74c419d9377381f15b69df0c
gem install rails <tuamigoro012@gmail.com>
2017-09-17 15:18:54 -0400

    Cerrar Sesion

--- Pasanaco/src/pages/home/home.ts
+++ Pasanaco/src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AuthService } from '../../providers/auth-service';
+
+import { LoginPage } from '../login/login';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    public authService: AuthService) {
   }
 
+  signOut() {
+    this.authService.signOut();
+    this.navCtrl.setRoot(LoginPage);
+  }
 }
