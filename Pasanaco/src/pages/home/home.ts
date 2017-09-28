d788ff56a501275a5755c97b70f5eb577dbdfab5
unknown <marcandea2@gmail.com>
2017-09-28 12:19:27 +0200

    Console: muestra objeto de usuario registrado

--- Pasanaco/src/pages/home/home.ts
+++ Pasanaco/src/pages/home/home.ts
@@ -5,6 +5,7 @@ import { AuthService } from '../../providers/auth-service';
 
 import { LoginPage } from '../login/login';
 import { CreargrupoPage } from '../creargrupo/creargrupo';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'page-home',
@@ -14,12 +15,18 @@ export class HomePage {
 
   constructor(
     public navCtrl: NavController,
-    public authService: AuthService) {
+    public authService: AuthService,
+    public af: AngularFireAuth
+    ) {
   }
 
   signOut() {
     this.authService.signOut();
     this.navCtrl.setRoot(LoginPage);
+  }
+
+  getUser(){
+    console.log(this.af.auth.currentUser.displayName);
   }
 
   
